e9dc617c10ee74e4aebb6f01998f3172c8f9ef90
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-27 15:32:31 -0300

    create classe domain

--- src/main/java/com/lucbecker/orderservice/domain/Cliente.java
+++ src/main/java/com/lucbecker/orderservice/domain/Cliente.java
@@ -1,14 +1,28 @@
 package com.lucbecker.orderservice.domain;
 
+import org.apache.tomcat.jni.OS;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cliente extends Pessoa implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    private List<OS> list = new ArrayList<>();
 
     public Cliente() {
     }
 
     public Cliente(Integer id, String nome, String cpf, String telefone) {
         super(id, nome, cpf, telefone);
+    }
+
+    public List<OS> getList() {
+        return list;
+    }
+
+    public void setList(List<OS> list) {
+        this.list = list;
     }
 }
--- src/main/java/com/lucbecker/orderservice/domain/Tecnico.java
+++ src/main/java/com/lucbecker/orderservice/domain/Tecnico.java
@@ -1,12 +1,27 @@
 package com.lucbecker.orderservice.domain;
 
-public class Tecnico extends Pessoa {
+import org.apache.tomcat.jni.OS;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tecnico extends Pessoa {
+    private static final long serialVersionUID = 1L;
+
+    private List<OS> list = new ArrayList<>();
     public Tecnico() {
     }
 
     public Tecnico(Integer id, String nome, String cpf, String telefone) {
         super(id, nome, cpf, telefone);
+    }
+
+    public List<OS> getList() {
+        return list;
+    }
+
+    public void setList(List<OS> list) {
+        this.list = list;
     }
 }
 
