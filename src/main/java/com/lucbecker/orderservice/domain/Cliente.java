201c50c7f12cc5b2e70eebde78ad0edbb1ba45a8
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-27 15:20:29 -0300

    create classe domain

---
+++ src/main/java/com/lucbecker/orderservice/domain/Cliente.java
@@ -0,0 +1,14 @@
+package com.lucbecker.orderservice.domain;
+
+import java.io.Serializable;
+
+public class Cliente extends Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String nome, String cpf, String telefone) {
+        super(id, nome, cpf, telefone);
+    }
+}
---
+++ src/main/java/com/lucbecker/orderservice/domain/Tecnico.java
@@ -0,0 +1,14 @@
+package com.lucbecker.orderservice.domain;
+
+public class Tecnico extends Pessoa {
+
+    public Tecnico() {
+    }
+
+    public Tecnico(Integer id, String nome, String cpf, String telefone) {
+        super(id, nome, cpf, telefone);
+    }
+}
+
+
+
