59d16c512d975844117c0eb4eca180aaaf0d8fed
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-27 16:37:35 -0300

    implentacao classes repository

---
+++ src/main/java/com/lucbecker/orderservice/repositories/TecnicoRepository.java
@@ -0,0 +1,9 @@
+package com.lucbecker.orderservice.repositories;
+
+import com.lucbecker.orderservice.domain.Tecnico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
+}
