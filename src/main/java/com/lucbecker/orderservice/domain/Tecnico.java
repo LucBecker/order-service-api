e4b4de7bfc29c357fa3bcaa681ee7d78cf50758f
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-27 23:56:50 -0300

    create DTO

--- src/main/java/com/lucbecker/orderservice/domain/Tecnico.java
+++ src/main/java/com/lucbecker/orderservice/domain/Tecnico.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Tecnico extends Pessoa {
+public class Tecnico extends Pessoa implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @JsonIgnore
--- src/main/java/com/lucbecker/orderservice/resources/TecnicoResource.java
+++ src/main/java/com/lucbecker/orderservice/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.lucbecker.orderservice.resources;
 
 import com.lucbecker.orderservice.domain.Tecnico;
+import com.lucbecker.orderservice.dto.TecnicoDTO;
 import com.lucbecker.orderservice.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,8 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
-        Tecnico obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
+        TecnicoDTO objDTO = new TecnicoDTO(service.findById(id));
+        return ResponseEntity.ok().body(objDTO);
     }
 }
