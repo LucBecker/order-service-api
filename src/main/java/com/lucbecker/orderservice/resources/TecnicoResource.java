418f1daaf656e6bbc8f4f0d0b860d7c697371827
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-29 12:26:12 -0300

    resource getAll

--- src/main/java/com/lucbecker/orderservice/resources/TecnicoResource.java
+++ src/main/java/com/lucbecker/orderservice/resources/TecnicoResource.java
@@ -1,6 +1,5 @@
 package com.lucbecker.orderservice.resources;
 
-import com.lucbecker.orderservice.domain.Tecnico;
 import com.lucbecker.orderservice.dto.TecnicoDTO;
 import com.lucbecker.orderservice.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
@@ -17,8 +19,15 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         TecnicoDTO objDTO = new TecnicoDTO(service.findById(id));
         return ResponseEntity.ok().body(objDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findAll() {
+        List<TecnicoDTO> listDTO = service.findAll()
+                .stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/lucbecker/orderservice/services/DBService.java
+++ src/main/java/com/lucbecker/orderservice/services/DBService.java
@@ -25,11 +25,15 @@ public class DBService {
     public void instanciaDB() {
 
         Tecnico t1 = new Tecnico(null, "Lucas Becker", "911.836.820-42", "(88) 98888-8888");
+        Tecnico t2 = new Tecnico(null, "Linus Torvalds", "787.811.400-53", "(88) 94545-4545");
         Cliente c1 = new Cliente(null, "Betina Campos", "936.792.010-52", "(88) 98888-7777");
 
         OS os1 = new OS(null, Prioridade.ALTA, "Trocar fonte do notebook", Status.ANDAMENTO, t1, c1);
 
-        tecnicoRepository.saveAll(Arrays.asList(t1));
+        t1.getList().add(os1);
+        c1.getList().add(os1);
+
+        tecnicoRepository.saveAll(Arrays.asList(t1, t2));
         clienteRepository.saveAll(Arrays.asList(c1));
         osRepository.saveAll(Arrays.asList(os1));
     }
