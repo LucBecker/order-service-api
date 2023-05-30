c1e624c9593ea21ab36dbb44ad238b0797855a61
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-30 15:09:29 -0300

    resource update

--- src/main/java/com/lucbecker/orderservice/services/TecnicoService.java
+++ src/main/java/com/lucbecker/orderservice/services/TecnicoService.java
@@ -34,6 +34,19 @@ public class TecnicoService {
         return repository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
     }
 
+    public Tecnico update(Integer id, TecnicoDTO objDTO) {
+        Tecnico oldObj = findById(id);
+
+        if(findByCPF(objDTO) != null && findByCPF(objDTO).getId() != id) {
+            throw new DataIntegrityViolationException("CPF já cadastrado na base de dados!");
+        }
+
+        oldObj.setNome(objDTO.getNome());
+        oldObj.setCpf(objDTO.getCpf());
+        oldObj.setTelefone(objDTO.getTelefone());
+        return repository.save(oldObj);
+    }
+
     private Tecnico findByCPF(TecnicoDTO objDTO){
         Tecnico obj = repository.findByCPF(objDTO.getCpf());
         if(obj != null){
