f011132a083010e5f35ff91794817d6cc0f496a0
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-31 15:45:30 -0300

    resource findAll OS

--- src/main/java/com/lucbecker/orderservice/resources/OsResource.java
+++ src/main/java/com/lucbecker/orderservice/resources/OsResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "os")
 public class OsResource {
@@ -21,5 +24,13 @@ public class OsResource {
     public ResponseEntity<OSDTO> findById(@PathVariable Integer id){
         OSDTO obj = new OSDTO(service.findById(id));
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OSDTO>> findAll(){
+        List<OSDTO> listDTO = service.findAll()
+                .stream().map(obj -> new OSDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+
     }
 }
--- src/main/java/com/lucbecker/orderservice/services/OsService.java
+++ src/main/java/com/lucbecker/orderservice/services/OsService.java
@@ -1,11 +1,13 @@
 package com.lucbecker.orderservice.services;
 
 import com.lucbecker.orderservice.domain.OS;
+import com.lucbecker.orderservice.dto.OSDTO;
 import com.lucbecker.orderservice.repositories.OSRepository;
 import com.lucbecker.orderservice.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +20,9 @@ public class OsService {
         Optional<OS> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: "+ OS.class.getName()));
+    }
+
+    public List<OS> findAll() {
+        return repository.findAll();
     }
 }
