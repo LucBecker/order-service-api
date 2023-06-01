6dadecbc5a9df0bbdf28adbfc0eb49076d02d67c
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-31 22:32:35 -0300

    cross origin

--- src/main/java/com/lucbecker/orderservice/resources/ClienteResource.java
+++ src/main/java/com/lucbecker/orderservice/resources/ClienteResource.java
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteResource {
